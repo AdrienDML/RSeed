b960e5d191cecf21b81836f5b392655ea28f9012
AdrienDML <adriendml99@gmail.com>
2021-05-27 18:05:39 +0200

    added the load as Cstr opition

--- crates/rseed_res/src/lib.rs
+++ crates/rseed_res/src/lib.rs
@@ -1,13 +1,14 @@
-use std::path::{Path, PathBuf};
+use std::{io::Read, path::{Path, PathBuf}};
 
 #[derive(Debug)]
 pub enum RessourceError {
     Directory(std::io::Error),
     PathDontExist,
+    FileContainsNullByte,
     File(std::io::Error),
 }
 
-pub type Result<T> = core::result::Result<T, RessourceError>;
+pub type Result<T> = std::result::Result<T, RessourceError>;
 
 pub struct RessourceLoader {
     res_path: PathBuf,
@@ -25,7 +26,27 @@ impl RessourceLoader {
     }
 
     pub fn load_file(&self, file_path: &Path) -> Result<std::fs::File> {
-        std::fs::File::open(self.res_path.join(file_path)).map_err(|e| RessourceError::File(e))
+        std::fs::File::open(self.res_path.join(file_path))
+            .map_err(|e| RessourceError::File(e))
+    }
+
+    pub fn load_as_cstring(&self, file_path : &Path) -> Result<std::ffi::CString> {
+        let mut file = std::fs::File::open(self.res_path.join(file_path))
+            .map_err(|e| RessourceError::File(e))?;
+
+        let mut content: Vec<u8> = Vec::with_capacity(
+            file.metadata()
+            .map_err(|e| RessourceError::File(e))?
+            .len() as usize + 1
+        );
+        file.read_to_end(&mut content)
+            .map_err(|e| RessourceError::File(e))?;
+
+        if content.iter().find(|i| **i == 0).is_some() {
+            return Err(RessourceError::FileContainsNullByte);
+        }
+
+        Ok(unsafe { std::ffi::CString::from_vec_unchecked(content) })
     }
 
     //pub fn load_shaders(&self, )
