613ec36af540c49d7cb4c0013509291401d2311d
Adrien <adriendml99@protonmail.com>
2021-07-15 04:15:39 +0100

    start replacing rseed_log by a fern logger in core

---
+++ crates/rseed_core/src/log.rs
@@ -0,0 +1,22 @@
+pub use log::{debug, error, info, trace, warn};
+
+struct Logger;
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
+
