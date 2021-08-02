1e839d19fc6ca71084fad8942cd28a6c2249320e
Adrien <adriendml99@protonmail.com>
2021-08-02 14:47:22 +0100

    added core

---
+++ crates/rseed_core/src/lib.rs
@@ -0,0 +1,30 @@
+pub mod consts;
+pub mod utils;
+mod log;
+pub mod private;
+
+
+// --------------------------
+// all the macros imported from external crates
+// --------------------------
+
+pub mod error {
+    pub use err_derive::*;
+}
+
+pub mod builder {
+    pub use derive_builder::{self,*};
+}
+
+pub mod serialization {
+    pub use serde::{self, Serialize, Deserialize};
+    pub use toml::{self,from_str, to_string, to_string_pretty};
+}
+
+pub mod prelude {
+    pub use crate::{
+        error::*,
+        builder::*,
+        serialization::*,
+    };
+}
---
+++ crates/rseed_core/src/log.rs
@@ -0,0 +1,20 @@
+pub use log::{debug, error, info, trace, warn};
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                crate::utils::get_time().unwrap(),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
+
